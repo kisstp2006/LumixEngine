781388303916040c89e88a61628f9f604a4ac272
Kiss Tibor Peter <kisstp2006@gmail.com>
2025-07-26 16:23:55 +0200

    Removed unused code from player_fps.lua

--- data/scripts/player_fps.lua
+++ data/scripts/player_fps.lua
@@ -70,15 +70,6 @@ function onControllerHit(obj)
     a:applyForce(dir)
 end
 
-function multiplyQuaternions(q1, q2)
-    return {
-        q1[4]*q2[1] + q1[1]*q2[4] + q1[2]*q2[3] - q1[3]*q2[2],
-        q1[4]*q2[2] - q1[1]*q2[3] + q1[2]*q2[4] + q1[3]*q2[1],
-        q1[4]*q2[3] + q1[1]*q2[2] - q1[2]*q2[1] + q1[3]*q2[4],
-        q1[4]*q2[4] - q1[1]*q2[1] - q1[2]*q2[2] - q1[3]*q2[3]
-    }
-end
-
 function update(td)
 
     local speed = 3 
